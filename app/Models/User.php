bff7772b3a587879b8ae54e9fd619a48d114c806
Marcio <marcioelias@gmail.com>
2020-05-19 13:09:40 -0300

    Criação de campo de primeiro data primeiro login

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,6 +37,7 @@ class User extends Authenticatable implements MustVerifyEmail
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'first_login_at' => 'datetime'
     ];
 
     /**
