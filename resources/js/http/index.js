773cbaed19abbc9a2e5cf5d008d02f017107aea4
Marcio <marcioelias@gmail.com>
2020-08-13 19:16:30 -0300

    compilação com url da amazon

--- resources/js/http/index.js
+++ resources/js/http/index.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 Vue.use({
     install(Vue) {
         Vue.prototype.$http = axios.create({
-            baseURL: 'https://mark2.dev.test',
+            baseURL: 'http://ec2-18-221-128-147.us-east-2.compute.amazonaws.com/',
             headers: {
                 common: {
                     'X-Requested-With': 'XMLHttpRequest'
