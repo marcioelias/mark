eba1a842aee605bcf2a280e3b66358412b9a9066
Marcio <marcioelias@gmail.com>
2020-11-19 12:03:16 -0300

    Correçao de bug ao cadastrar usuário ativo.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -110,8 +110,12 @@ class UserController extends Controller
             'password' => 'required|min:6|max:30|confirmed'
         ]);
 
+        Log::debug($request->all());
         $user = new User($request->all());
+        $user->active = ($request->active ?? false) ? true : false;
         $user->password = bcrypt($request->password);
+
+        Log::debug($user);
 
         if ($user->save()) {
             $tempPassword = new TempPassword;
